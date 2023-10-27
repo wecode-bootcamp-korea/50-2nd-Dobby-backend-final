529ba2fcc2d74f9d45951b974fad8816e94becd5
usnimoj <jominsu0103@naver.com>
2023-10-27 13:32:55 +0900

    test

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const routes = require("./routers");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+const server = http.createServer(app);
+const port = process.env.PORT;
+
+const start = async () => {
+  try {
+    server.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
