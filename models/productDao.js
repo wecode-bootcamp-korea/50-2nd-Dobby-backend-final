e51eea9c7ab27e6c785593d0c40d30f3263db72e
usnimoj <jominsu0103@naver.com>
2023-11-02 00:43:27 +0900

    쿼리문 대문자로 변경

--- models/productDao.js
+++ models/productDao.js
@@ -2,7 +2,11 @@ const appDataSource = require("./dataSource");
 
 const getAllProducts = async (req, res) => {
   const result = await appDataSource.query(
-    `select p.id , p.image , p.name , p.price , avg(c.score) as score , count(c.id) as commentCount from products p join comments c on p.id = c.products_id group by p.id , p.image , p.name , p.price`
+    `SELECT p.id , p.image , p.name , p.price , AVG(c.score) AS score , count(c.id) AS commentCount 
+    FROM products p 
+    JOIN comments c 
+    ON p.id = c.products_id 
+    GROUP BY p.id , p.image , p.name , p.price`
   );
   return result;
 };
