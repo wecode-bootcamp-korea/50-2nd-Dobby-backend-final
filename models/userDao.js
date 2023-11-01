0255d22c016e8e5853d9adbfc0aa5f6629f27f6f
coderjins <coderjins@gmail.com>
2023-11-01 19:16:21 +0900

    1차 피드백 반영 코드,추가기능 없는 코드

--- models/userDao.js
+++ models/userDao.js
@@ -6,12 +6,6 @@ const findUserByEmail = async (email) => {
   return await appDataSource.query(query);
 };
 
-const findUserByPhonenumber = async (phoneNumber) => {
-  let formattedNumber = phoneNumber.startsWith("0") ? phoneNumber.substring(1) : phoneNumber;
-  const query = `SELECT * FROM users WHERE phonenumber = '${formattedNumber}'`;
-  return await appDataSource.query(query);
-}
-
 const createUser = async (email, hashedPw, name, phonenumber, nickname) => {
   const query = `
     INSERT INTO users (
@@ -33,62 +27,9 @@ const createUser = async (email, hashedPw, name, phonenumber, nickname) => {
   return await appDataSource.query(query);
 };
 
-const insertEmaiAuth = async (email, number) => {
-  const query = `
-    INSERT INTO auth_table (email, auth_code) 
-    VALUES ('${email}', '${number}')
-  `;
-  return await appDataSource.query(query);
-};
 
-const insertPhoneAuth = async (phoneNumber, number) => {
-  const query = `
-    INSERT INTO auth (phonenumber, auth_code) 
-    VALUES ('${phoneNumber}', '${number}')
-  `;
-  return await appDataSource.query(query);
-};
-
-const getAuthNumberByEmail = async (email) => {
-  const query = `SELECT auth_code FROM auth_table WHERE email = '${email}'`;
-  const rows = await appDataSource.query(query);
-
-  if (rows.length === 0) return null;
-  return rows[0].auth_number;
-};
-
-const getAuthNumberByPhoneNumber= async (phoneNumber) => {
-  const query = `SELECT auth_code FROM auth WHERE phonenumber = '${phoneNumber}'`;
-  const rows = await appDataSource.query(query);
-
-  if (rows.length === 0) return null;
-  return rows[0].auth_code;
-};
-
-const updatePassword = async (email, hashedPassword) => {
-  const query = `UPDATE users SET password = '${hashedPassword}' WHERE email = '${email}'`;
-  await appDataSource.query(query);
-};
-
-const deleteUserAuthNumberByEmail = async (email) => {
-  const query = "DELETE FROM auth WHERE email = ?";
-  await appDataSource.query(query, [email]);
-};
-
-const deleteUserAuthNumberByPhoneNumber = async (phoneNumber) => {
-  const query = "DELETE FROM auth WHERE phonenumber = ?";
-  await appDataSource.query(query, [phoneNumber]);
-};
 
 module.exports = { 
   createUser,
-  findUserByEmail, 
-  insertEmaiAuth, 
-  getAuthNumberByEmail, 
-  updatePassword,
-  deleteUserAuthNumberByEmail,
-  deleteUserAuthNumberByPhoneNumber,
-  findUserByPhonenumber,
-  insertPhoneAuth,
-  getAuthNumberByPhoneNumber
+  findUserByEmail
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,11 +5,4 @@ const userController = require('../controllers/userController');
 router.post('/signup', userController.signUp);
 router.post('/login', userController.logIn);
 
-router.post('/emailauth',userController.emailAuth);
-router.post('/emailauth/emailverifyNumber',userController.emailVerifyNumber);
-
-router.post('/phoneauth',userController.phoneAuth);
-router.post('/phoneauth/phoneverifynumber',userController.phoneVerifyNumber);
-
-
 module.exports.router = router;
